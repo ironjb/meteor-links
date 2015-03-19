f10135e8c397d5fe653207ea5b458b5935dcb53b
Joe Bell <ironjb20@gmail.com>
2015-03-18 19:01:02 -0600

    Added Basic Login Modal

--- meteor_links.js
+++ meteor_links.js
@@ -1,9 +1,32 @@
 // Router.configure({layoutTemplate: 'MainLayout'});
-Router.route('/', function () { this.layout('main_layout'); this.render('home'); });
+Router.route('/', function () { this.layout('mainLayout'); this.render('home'); });
 Router.route('admin', function () { this.render('admin'); });
 
 if (Meteor.isClient) {
-	// counter starts at 0
+	Template.loginModal.events({
+		'submit #loginForm': function (event, target) {
+			event.preventDefault();
+			// window.console.log && console.log('event', event, 'target', target);
+			// window.console.log && console.log(event.target.loginEmail.value);
+			// window.console.log && console.log(event.target.loginPassword.value);
+			var email = event.target.loginEmail.value
+			, password = event.target.loginPassword.value;
+
+			// Trim and validate fields here
+
+			// If validation passes, supply the appropriate fields to the
+			// Meteor.loginWithPassword() function.
+			Meteor.loginWithPassword(email, password, function (err) {
+				if (err) {
+					// Login failed.
+				} else {
+					// User logged in.
+				}
+			});
+			return false;
+		}
+	});
+	/*// counter starts at 0
 	Session.setDefault('counter', 0);
 
 	Template.home.helpers({
@@ -17,7 +40,7 @@ if (Meteor.isClient) {
 			// increment the counter when button is clicked
 			Session.set('counter', Session.get('counter') + 1);
 		}
-	});
+	});*/
 }
 
 if (Meteor.isServer) {
